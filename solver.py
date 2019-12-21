8c0bb43b56bc8bf4d7f782e078d126720ca53c77
scarmo <scarmody@tcd.ie>
2019-12-21 15:58:55 +0000

    Finished solver and moved class to seperate file

--- solver.py
+++ solver.py
@@ -1,56 +1,8 @@
-from random import randint
-
-class Sudoku:
-    def generatePuzzle(self):
-        ## Make a valid sudoku board
-
-        # 1. generate first row
-        validRow = [1,2,3,4,5,6,7,8,9]
-        for i in range(0,9):
-            randPos = randint(1,9) - 1
-            validRow[i], validRow[randPos] = validRow[randPos], validRow[i]
-
-        # 2. further rows are simply a rotation of the first row
-        for i in range(0,9,3):
-            for j in range(i, i+3):
-                self.board[j] = validRow
-                validRow = validRow[3:] + validRow[:3]
-            validRow = self.board[i]
-            validRow = validRow[1:] + validRow[:1]
-
-        # 3. Remove some numbers
-
-
-    def __init__(self):
-        self.board = [[0,0,0,0,0,0,0,0,0],
-                     [0,0,0,0,0,0,0,0,0],
-                     [0,0,0,0,0,0,0,0,0],
-                     [0,0,0,0,0,0,0,0,0],
-                     [0,0,0,0,0,0,0,0,0],
-                     [0,0,0,0,0,0,0,0,0],
-                     [0,0,0,0,0,0,0,0,0],
-                     [0,0,0,0,0,0,0,0,0],
-                     [0,0,0,0,0,0,0,0,0]]
-
-        self.generatePuzzle()
-
-    def prettyPrint(self):
-        for row in self.board:
-            print(row)
-
-    def check_box(self, col, row):
-        rowMin = (row // 3) * 3
-        colMin = (col // 3) * 3
-
-
-
+from Sudoku import Sudoku
 
 if __name__ == "__main__":
     b = Sudoku()
     b.prettyPrint()
-
-
-
-
-
+    print(b.solveBoard())
+    b.prettyPrint()
 
